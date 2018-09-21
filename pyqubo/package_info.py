b33e19bb4c2cb620872922cc5781d3bc5b2d6b35
Kotaro Tanahashi <tanahashi@r.recruit.co.jp>
2018-09-22 01:33:31 +0900

    Enabled CI

--- pyqubo/package_info.py
+++ pyqubo/package_info.py
@@ -1,6 +1,6 @@
 # (major, minor, patch, prerelease)
 
-VERSION = (0, 0, 4, "")
+VERSION = (0, 0, 5, "")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
