db548b7562bf5f2b4d1cb5d9c88d7f457897f768
kotarotanahashi <colon0722@gmail.com>
2018-10-04 11:00:42 +0900

    release v0.0.7

--- pyqubo/package_info.py
+++ pyqubo/package_info.py
@@ -1,6 +1,6 @@
 # (major, minor, patch, prerelease)
 
-VERSION = (0, 0, 6, "")
+VERSION = (0, 0, 7, "")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
