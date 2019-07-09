5a13ef6f115d9219691602e8e3ee42657fb1fb17
Mashi <Mashiyat_Zaman@r.recruit.co.jp>
2019-07-09 12:04:05 +0900

    update test

--- test/test_model.py
+++ test/test_model.py
@@ -59,22 +59,20 @@ class TestModel(unittest.TestCase):
         exp = 1 + a * b + c * (a - 2)
         model = exp.compile()
         var_set = VarSetFromVarLabels([b])
-        # sample = solve_qubo(model.to_qubo(feed_dict={'c': 3.0})[0])
         sample_sol = {'a': 0, 'b': 0}
         sub_qubo, offset = model.sub_qubo(var_set, sample_sol, feed_dict={'c': 3.0})
-        assert_qubo_equal(sub_qubo, {("b", "b"): 0.0})
+        self.assertTrue(sub_qubo == {("b", "b"): 0.0})
         self.assertTrue(offset == -5.0)
 
     def test_sub_ising(self):
         a, b, c = Binary("a"), Binary("b"), Placeholder("c")
         exp = 1 + a * b + c * (a - 2)
         model = exp.compile()
-        var_set = VarSetFromVarLabels([a])
-        # sample = solve_qubo(model.to_qubo(feed_dict={'c': 3.0})[0])
+        var_set = VarSetFromVarLabels([b])
         sample_sol = {'a': 0, 'b': 0}
         linear, quad, offset = model.sub_ising(var_set, sample_sol, feed_dict={'c': 3.0})
         self.assertTrue(linear == {'b': 0.0})
-        assert_qubo_equal(quad, {})
+        self.assertTrue(quad == {})
         self.assertTrue(offset == -5.0)
 
     def test_decode(self):
