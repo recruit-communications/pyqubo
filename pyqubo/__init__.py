fa86b56002f3880e165a65161856ccfe2ddfbd57
tanahashi-kotaro <tanahashi@r.recruit.co.jp>
2020-09-23 14:53:51 +0900

    add logic expression

--- pyqubo/__init__.py
+++ pyqubo/__init__.py
@@ -6,6 +6,7 @@ from pyqubo.utils.asserts import *
 from pyqubo.utils.solver import *
 from .array import *
 from .logical_constraint import *
+from .logic import *
 from pyqubo.integer.integer import *
 from pyqubo.integer.log_encoded_integer import *
 from pyqubo.integer.one_hot_enc_integer import *
