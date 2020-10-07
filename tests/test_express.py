6ffdfa4056de7607fc5ee6899aec564fc91e8876
kotarotanahashi <colon0722@gmail.com>
2020-10-08 01:28:09 +0900

    test model

--- tests/test_express.py
+++ tests/test_express.py
@@ -109,7 +109,7 @@ class TestExpress(unittest.TestCase):
         import dimod
         a, b, c = Binary("a"), Binary("b"), Binary("c")
         #exp = SubH(a*b*c, label="constraint")
-        exp=a*b*c
+        exp=a*b
         expected_qubo = {('a', 'a'): 1.0, ('b', 'b'): 1.0}
         expected_offset = 0
         model = exp.compile()
