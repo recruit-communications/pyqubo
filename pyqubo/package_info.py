b8c5dad7c6ad6fe125055a6f6de7916bd4600204
kotarotanahashi <colon0722@gmail.com>
2020-10-20 03:27:15 +0900

    update version to 1.0.6

--- pyqubo/package_info.py
+++ pyqubo/package_info.py
@@ -1,6 +1,6 @@
 # (major, minor, patch, prerelease)
 
-VERSION = (1, 0, 5, "")
+VERSION = (1, 0, 6, "")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
