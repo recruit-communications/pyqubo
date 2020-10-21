ef4aa9bb781426211ead0022b5b2b9c4b3941539
j-i-k-o <jiko.rogy@gmail.com>
2020-10-21 13:37:18 +0900

    update pybind11 version

--- external/pybind11.cmake
+++ external/pybind11.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     pybind11
     GIT_REPOSITORY  https://github.com/pybind/pybind11
-    GIT_TAG         v2.2.4
+    GIT_TAG         v2.5.0
 )
 
 FetchContent_GetProperties(pybind11)
