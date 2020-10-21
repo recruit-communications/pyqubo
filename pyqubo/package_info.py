2234cd8267900d0d658315a3aea497fccc41f750
Kotaro Tanahashi <colon0722@gmail.com>
2020-10-21 16:42:06 +0900

    update version to 1.0.7

--- pyqubo/package_info.py
+++ pyqubo/package_info.py
@@ -1,6 +1,6 @@
 # (major, minor, patch, prerelease)
 
-VERSION = (1, 0, 6, "")
+VERSION = (1, 0, 7, "")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
