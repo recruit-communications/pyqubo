40b08867d2c6c94fc0e83d0207935a304d4bd42e
kotarotanahashi <colon0722@gmail.com>
2021-01-21 11:40:48 +0900

    test deploy linux and osx

--- pyqubo/package_info.py
+++ pyqubo/package_info.py
@@ -1,6 +1,6 @@
 # (major, minor, patch, prerelease)
 
-VERSION = (1, 0, 8, "")
+VERSION = (1, 0, 8, "t1")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
