21d3c48ad84ea3b7773cb077618ecbdbe4d01798
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-14 20:20:15 +0900

    Update pybind11.cmake

--- external/pybind11.cmake
+++ external/pybind11.cmake
@@ -4,14 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     pybind11
     GIT_REPOSITORY  https://github.com/pybind/pybind11
-    GIT_TAG         v2.5.0
+    GIT_TAG         v2.6.2
 )
-
-FetchContent_GetProperties(pybind11)
-if(NOT pybind11_fetch_POPULATED)
-    message(STATUS "Fetch pybind11 for python-binding")
-    FetchContent_Populate(pybind11)
-    add_subdirectory(${pybind11_SOURCE_DIR})
-endif()
 
 FetchContent_MakeAvailable(pybind11)
