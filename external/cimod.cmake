15a8b51a9f0d748fce7ce5373f85601e1991d7d5
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-14 20:18:22 +0900

    Update cimod.cmake

--- external/cimod.cmake
+++ external/cimod.cmake
@@ -3,15 +3,8 @@ include(FetchContent)
 #### cimod ####
 FetchContent_Declare(
     cimod
-    GIT_REPOSITORY  https://github.com/kotarotanahashi/cimod.git
-    GIT_TAG v1.0.0
+    GIT_REPOSITORY  https://github.com/OpenJij/cimod.git
+    GIT_TAG  v1.2.3
 )
-
-FetchContent_GetProperties(cimod)
-if(NOT cimod_POPULATED)
-    message(STATUS "Fetch cimod")
-    FetchContent_Populate(cimod)
-    include_directories(${cimod_SOURCE_DIR}/src)
-endif()
 
 FetchContent_MakeAvailable(cimod)
