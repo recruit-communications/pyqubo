a1b60c3b73d3cf388e3ed59d7551813fdfcf53a0
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-26 01:13:55 +0900

    Update googletest.cmake

--- external/googletest.cmake
+++ external/googletest.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 #### Google test ####
 FetchContent_Declare(
     googletest
-    GIT_REPOSITORY  https://github.com/google/googletest.git
+    GIT_REPOSITORY  https://github.com/google/googletest
     GIT_TAG         release-1.11.0
 )
 
