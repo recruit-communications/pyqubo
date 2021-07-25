7fab517d99637469e0b62ae909e2baedc86ece00
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-26 01:52:48 +0900

    Update cimod.cmake

--- external/cimod.cmake
+++ external/cimod.cmake
@@ -12,6 +12,5 @@ FetchContent_MakeAvailable(cimod)
 add_library(cxxcimod_header_only INTERFACE)
 target_include_directories(cxxcimod_header_only INTERFACE ${cimod_SOURCE_DIR})
 target_link_libraries(cxxcimod_header_only INTERFACE 
-  $<IF:$<TARGET_EXISTS:Eigen3::Eigen>,Eigen3::Eigen,eigen_lib>
   $<$<TARGET_EXISTS:OpenMP::OpenMP_CXX>:OpenMP::OpenMP_CXX>
 )
