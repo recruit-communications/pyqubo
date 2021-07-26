d8337cfbacc9a6bd94b24eb591ed61b9c2bd3e06
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-26 14:34:09 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -1,10 +1,10 @@
 #### Eigen ####
 FetchContent_Declare(
-    eigen
+    pyqubo-eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG         3.3.9
     CMAKE_ARGS -DEIGEN_MPL2_ONLY
     )
 set(EIGEN_MPL2_ONLY ON)
 set(EIGEN_CPP_STANDARD 11)
-FetchContent_MakeAvailable(eigen)
+FetchContent_MakeAvailable(pyqubo-eigen)
