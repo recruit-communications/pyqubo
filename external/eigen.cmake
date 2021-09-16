3abf07f8c600736052ab27950ec970099691a529
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 18:04:28 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -6,10 +6,4 @@ FetchContent_Declare(
     GIT_TAG        3.3.7
 )
 
-FetchContent_GetProperties(eigen)
-
-if(NOT eigen_POPULATED)
-    message(STATUS "Fetch eigen")
-    FetchContent_Populate(eigen)
-    include_directories(${eigen_SOURCE_DIR})
-endif()
+FetchContent_MakeAvailable(eigen)
