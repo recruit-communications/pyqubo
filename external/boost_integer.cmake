35a4af23eaf56309fe421f7d9bed048e09fdc102
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 19:04:50 +0900

    Update boost_integer.cmake

--- external/boost_integer.cmake
+++ external/boost_integer.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     boost_integer
-    GIT_REPOSITORY https://github.com/boostorg/integer
-    GIT_TAG        boost-1.76.0
+    GIT_REPOSITORY ${PREFIX_BOOST_URL}/integer
+    GIT_TAG        ${PREFIX_BOOST_TAG}
 )
 
 FetchContent_MakeAvailable(boost_integer)
