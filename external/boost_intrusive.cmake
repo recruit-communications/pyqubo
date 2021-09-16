5efef51f841028125c2ea59e525011fb29b5a795
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 19:05:32 +0900

    Update boost_intrusive.cmake

--- external/boost_intrusive.cmake
+++ external/boost_intrusive.cmake
@@ -2,8 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(
     boost_intrusive
-    GIT_REPOSITORY https://github.com/boostorg/intrusive
-    GIT_TAG        boost-1.76.0
+    GIT_REPOSITORY ${PREFIX_BOOST_URL}/intrusive
+    GIT_TAG        ${PREFIX_BOOST_TAG}
 )
 
-FetchContent_MakeAvailable(boost_intrusive)
