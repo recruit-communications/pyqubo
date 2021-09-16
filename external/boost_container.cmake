8938dd3a69bc502091596efdcf2f9a8a69aabae3
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 19:46:53 +0900

    Update boost_container.cmake

--- external/boost_container.cmake
+++ external/boost_container.cmake
@@ -2,7 +2,14 @@ include(FetchContent)
 
 FetchContent_Declare(
     boost_container
-    GIT_REPOSITORY ${PREFIX_BOOST_URL}/container
-    GIT_TAG        ${PREFIX_BOOST_TAG}
+    GIT_REPOSITORY https://github.com/boostorg/container
+    GIT_TAG        boost-1.76.0
 )
 
+FetchContent_GetProperties(boost_container)
+
+if(NOT boost_container_POPULATED)
+    message(STATUS "Fetch boost container")
+    FetchContent_Populate(boost_container)
+    include_directories(${boost_container_SOURCE_DIR}/include)
+endif()
