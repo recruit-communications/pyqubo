95bf83409fcdc69d6986b96328b11615c94605b5
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 19:48:20 +0900

    Update boost_detail.cmake

--- external/boost_detail.cmake
+++ external/boost_detail.cmake
@@ -2,7 +2,14 @@ include(FetchContent)
 
 FetchContent_Declare(
     boost_detail
-    GIT_REPOSITORY ${PREFIX_BOOST_URL}/detail
-    GIT_TAG        ${PREFIX_BOOST_TAG}
+    GIT_REPOSITORY https://github.com/boostorg/detail
+    GIT_TAG        boost-1.76.0
 )
 
+FetchContent_GetProperties(boost_detail)
+
+if(NOT boost_detail_POPULATED)
+    message(STATUS "Fetch boost detail")
+    FetchContent_Populate(boost_detail)
+    include_directories(${boost_detail_SOURCE_DIR}/include)
+endif()
