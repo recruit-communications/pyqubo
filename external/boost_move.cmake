729d6aa40409f931ea5f9b351e2b5009c8bc9362
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 18:02:44 +0900

    Update boost_move.cmake

--- external/boost_move.cmake
+++ external/boost_move.cmake
@@ -6,10 +6,4 @@ FetchContent_Declare(
     GIT_TAG        boost-1.76.0
 )
 
-FetchContent_GetProperties(boost_move)
-
-if(NOT boost_move_POPULATED)
-    message(STATUS "Fetch boost move")
-    FetchContent_Populate(boost_move)
-    include_directories(${boost_move_SOURCE_DIR}/include)
-endif()
+FetchContent_MakeAvailable(boost_move)
