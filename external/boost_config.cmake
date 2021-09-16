fd1fe66e3868459c536097bb4b20706fb0041c70
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 19:46:27 +0900

    Update boost_config.cmake

--- external/boost_config.cmake
+++ external/boost_config.cmake
@@ -2,7 +2,14 @@ include(FetchContent)
 
 FetchContent_Declare(
     boost_config
-    GIT_REPOSITORY ${PREFIX_BOOST_URL}/config
-    GIT_TAG        ${PREFIX_BOOST_TAG}
+    GIT_REPOSITORY https://github.com/boostorg/config
+    GIT_TAG        boost-1.76.0
 )
 
+FetchContent_GetProperties(boost_config)
+
+if(NOT boost_config_POPULATED)
+    message(STATUS "Fetch boost config")
+    FetchContent_Populate(boost_config)
+    include_directories(${boost_config_SOURCE_DIR}/include)
+endif()
