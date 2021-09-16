9e5b08787e3c98c905acbd0e489dea9ec56082b6
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 18:03:36 +0900

    Update boost_type_traits.cmake

--- external/boost_type_traits.cmake
+++ external/boost_type_traits.cmake
@@ -6,10 +6,4 @@ FetchContent_Declare(
     GIT_TAG        boost-1.76.0
 )
 
-FetchContent_GetProperties(boost_type_traits)
-
-if(NOT boost_type_traits_POPULATED)
-    message(STATUS "Fetch boost type_traits")
-    FetchContent_Populate(boost_type_traits)
-    include_directories(${boost_type_traits_SOURCE_DIR}/include)
-endif()
+FetchContent_MakeAvailable(boost_type_traits)
