d7c23e41eb71e66c51fbacaca534018105251186
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 19:47:52 +0900

    Update boost_core.cmake

--- external/boost_core.cmake
+++ external/boost_core.cmake
@@ -2,6 +2,14 @@ include(FetchContent)
 
 FetchContent_Declare(
     boost_core
-    GIT_REPOSITORY ${PREFIX_BOOST_URL}/core
-    GIT_TAG        ${PREFIX_BOOST_TAG}
+    GIT_REPOSITORY https://github.com/boostorg/core
+    GIT_TAG        boost-1.76.0
 )
+
+FetchContent_GetProperties(boost_core)
+
+if(NOT boost_core_POPULATED)
+    message(STATUS "Fetch boost core")
+    FetchContent_Populate(boost_core)
+    include_directories(${boost_core_SOURCE_DIR}/include)
+endif()
