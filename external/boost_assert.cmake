60d2061c7575098d4a5f6964c0bd29f0ab7ba00c
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-09-16 19:45:49 +0900

    Update boost_assert.cmake

--- external/boost_assert.cmake
+++ external/boost_assert.cmake
@@ -2,6 +2,14 @@ include(FetchContent)
 
 FetchContent_Declare(
     boost_assert
-    GIT_REPOSITORY ${PREFIX_BOOST_URL}/assert
-    GIT_TAG        ${PREFIX_BOOST_TAG}
+    GIT_REPOSITORY https://github.com/boostorg/assert
+    GIT_TAG        boost-1.76.0
 )
+
+FetchContent_GetProperties(boost_assert)
+
+if(NOT boost_assert_POPULATED)
+    message(STATUS "Fetch boost assert")
+    FetchContent_Populate(boost_assert)
+    include_directories(${boost_assert_SOURCE_DIR}/include)
+endif()
