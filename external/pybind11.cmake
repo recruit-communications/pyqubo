a85fa96ff93d7b67fa300cab60523472be22160d
Kotaro Tanahashi <colon0722@gmail.com>
2022-12-14 05:35:10 +0900

    update pybind version

--- external/pybind11.cmake
+++ external/pybind11.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     pybind11
     GIT_REPOSITORY https://github.com/pybind/pybind11
-    GIT_TAG        v2.6.2
+    GIT_TAG        v2.10.1
 )
 
 FetchContent_MakeAvailable(pybind11)
