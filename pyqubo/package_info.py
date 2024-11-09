717c680dae392e769191f359e41c57445137af5d
Kotaro Tanahashi <colon0722@gmail.com>
2024-11-09 17:46:04 +0900

    Feature/v1.5.0 (#234)
    * update python to 311, 312
    
    * update github actions
    
    * deprecate Python 3.6 3.7
    
    * update cibuildwheels
    
    * fix setup.py
    
    * update
    
    * fix upload-artifact
    
    * update boost version
    
    * use c++14
    
    * add BOOST_NO_CXX98_FUNCTION_BASE
    
    * use download-artifact@v2
    
    * use download-artifact@v3
    
    * remove i686
    
    * update badge
    
    * fix readme
    
    * update version to 1.5.0

--- pyqubo/package_info.py
+++ pyqubo/package_info.py
@@ -1,6 +1,6 @@
 # (major, minor, patch, prerelease)
 
-VERSION = (1, 4, 0, "")
+VERSION = (1, 5, 0, "")
 __shortversion__ = '.'.join(map(str, VERSION[:3]))
 __version__ = '.'.join(map(str, VERSION[:3])) + "".join(VERSION[3:])
 
